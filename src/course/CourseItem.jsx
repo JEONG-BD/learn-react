8c87f343c1e91385ec6e5ab76df978989c5b777a
JEONG-BD <dev.byeongdu@gmail.com>
2026-08-18 23:14:32 +0900

    conditional rendering

--- src/course/CourseItem.jsx
+++ src/course/CourseItem.jsx
@@ -1,5 +1,44 @@
+// 1. 
+// function HeartBtn({isFavorite=false}){
+//   if(isFavorite){
+//     return (
+//         <button className="btn">
+//           <img className="icon-heart" src="/img/heart-fill-icon.svg"/> 
+//           <i className="fa-solid fa-heart"></i>
+//         </button>
+//     )
+//   }
+//   return (
+//     <button className="btn">
+//       <img className="icon-heart" src="/img/heart-icon.svg"/> 
+//       <i className="fa-solid fa-heart"></i>
+//     </button>
+//   )
+// }
 
-function CourseItem({title, description, thumbnail}) {
+// 2. 
+// function HeartBtn({isFavorite = false}){
+//   return (
+//     <button className="btn">
+//       {isFavorite ? (
+//           <img className="icon-heart" src="/img/heart-fill-icon.svg"/>   
+//         ) : (
+//           <img className="icon-heart" src="/img/heart-icon.svg"/> 
+//         )}
+//     </button>
+//     )
+//   }
+
+function HeartBtn({isFavorite = false}){
+  return (
+    <button className="btn">
+          <img className="btn__img" src={isFavorite ? "/img/heart-fill-icon.svg" : "/img/heart-icon.svg"}/>     
+    </button>
+    )
+  }
+
+
+function CourseItem({title, description, thumbnail, isFavorite, link}) {
   // const course = {
   //   title: '입문자를 위한, HTML&CSS 웹 개발 입문',
   //   description: '웹 개발에 필요한 기본 지식을 배웁니다. ',
@@ -26,12 +65,14 @@ function CourseItem({title, description, thumbnail}) {
           <div className="course__description">{description}</div>
         </div>
         <div className="course__icons">
-          <button className="btn">
-              <i className="fa-solid fa-heart"></i>
-          </button>
-          <a className="btn" href="https://inf.run/JxyyT" target="_blank" rel="noreferrer">
-              <i className="fa-solid fa-link"></i>
-          </a>
+          <HeartBtn isFavorite={isFavorite}/>
+          <i className="fa-solid fa-heart"></i>
+          {link && (
+            <a className="btn" href="https://inf.run/JxyyT" target="_blank" rel="noreferrer">
+              <img className="btn__img" src="/img/link-icon.svg" alt=""/>
+            </a>
+          )}
+          
         </div>
       </article>
   )
